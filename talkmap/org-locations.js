f32dce7c12900e2db462093939519e98f83bc40e
Ale <alessandro.sanzeni@gmail.com>
2025-09-23 06:09:44 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,127 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
+    "Seminar<br />Columbia University \u2014 Center for Theoretical Neuroscience; New York, NY, United States",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Conference 'Analysis and Modeling of Connectomes'<br />Janelia Research Campus; Ashburn, VA, United States",
+    39.029784,
+    -77.4744148
+  ],
+  [
+    "Seminar<br />University of Geneva \u2014 Department of Basic Neurosciences; Geneva, Switzerland",
+    46.2017559,
+    6.1466014
+  ],
+  [
+    "First Conference of the Italian Network for Computational Neuroscience (INCN)<br />Istituto Superiore di Sanit\u00e0 (ISS); Rome, Italy",
+    41.8933203,
+    12.4829321
+  ],
+  [
+    "Seminar<br />Bocconi University \u2014 Department of Decision Sciences; Milan, Italy",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "Workshop 'Computational Aspects and Modeling of Biological Information'<br />Bocconi University; Milan, Italy",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "Tutorial 'Modeling cortical networks dynamics'<br />CNS 2024; Natal, Brazil",
+    -5.805398,
+    -35.2080905
+  ],
+  [
+    "Junior Scientists Workshop on Recent Advances in Theoretical Neuroscience<br />ICTP; Trieste, Italy",
+    45.6496485,
+    13.7772781
+  ],
+  [
+    "Statphys Satellite Workshop 'Dynamics and Complexity'<br />CNR Research Area; Pisa, Italy",
+    43.4714722,
+    10.6797912
+  ],
+  [
+    "Seminar<br />SISSA; Trieste, Italy",
+    45.6496485,
+    13.7772781
+  ],
+  [
+    "Seminar<br />National Institute of Mental Health (NIH); Bethesda, MD, United States",
+    38.9812726,
+    -77.1233587
+  ],
+  [
+    "Workshop 'Information processing through correlated and coordinated responses'<br />Bernstein Conference; Berlin, Germany",
+    52.5173885,
+    13.3951309
+  ],
+  [
+    "Statphys Satellite Workshop 'Perspectives in Nonlinear Dynamics'<br />IMT School for Advanced Studies; Lucca, Italy",
+    44.0177639,
+    10.45443
+  ],
+  [
+    "Seminar<br />Imperial College London; London, United Kingdom",
     51.4893335,
-    -0.14405508452768728
+    -0.1440551
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Seminar<br />\u00c9cole Normale Sup\u00e9rieure (ENS) \u2014 Department of Physics; Paris, France",
+    48.8534951,
+    2.3483915
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Seminar<br />University of Chicago; Chicago, IL, United States",
+    41.8755616,
+    -87.6244212
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Workshop 'Multi-Scale Brain Function and Neuro\u2011inspired devices'<br />Universit\u00e0 degli Studi di Milano; Milan, Italy",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "van Vreeswijk Theoretical Neuroscience Seminar<br />Online; Online",
+    43.5904719,
+    3.8595132
+  ],
+  [
+    "Seminar<br />Institute for Advanced Study (IAS); Princeton, NJ, United States",
+    40.3496953,
+    -74.6597376
+  ],
+  [
+    "Workshop 'The structure\u2011function binomial of cortical circuits across multiple scales'<br />CNS 2024; Natal, Brazil",
+    -5.805398,
+    -35.2080905
+  ],
+  [
+    "Seminar<br />LMU Biocenter; Munich, Germany",
+    48.1371079,
+    11.5753822
+  ],
+  [
+    "Seminar<br />Emory University; Atlanta, GA, United States",
+    33.7544657,
+    -84.3898151
+  ],
+  [
+    "Invited Seminar<br />National Institute of Mental Health (NIH); Bethesda, MD, United States",
+    38.9812726,
+    -77.1233587
+  ],
+  [
+    "Seminar<br />Gatsby Computational Neuroscience Unit, UCL; London, United Kingdom",
+    51.4893335,
+    -0.1440551
+  ],
+  [
+    "Gatsby Tri\u2011Center Annual Meeting<br />ELSC, Hebrew University of Jerusalem; Jerusalem, Israel",
+    31.7788472,
+    35.2257856
   ]
 ];
